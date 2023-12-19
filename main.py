6a1faa86f308ee9dc126aef518724b3bd129b52e
Xi HuaJun <12112806@main.sustech.edu.cn>
2023-12-19 14:25:22 +0800

    initial commit

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from cache.utils import build_classifier
 model = build_classifier("resnet50")
 trainloader, testloader = build_dataloader()
 train_feature, train_logits, train_labels = model.run_and_cache_outputs(trainloader)
-test_feature, test_logits, test_labels = model.run_and_cache_outputs(testloader)
+test_feature, test_logits, test_labels = model.run_and_cache_outputs(testloader, train=False)
