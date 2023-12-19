d31b15b03b793a3073005ee3913d717566df832d
Xi HuaJun <12112806@main.sustech.edu.cn>
2023-12-19 16:13:22 +0800

    initial commit

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
     parser = argparse.ArgumentParser(description="Experiment for 'How-KNN-Improve-the-Reliability-of-DNN'")
     parser.add_argument('--seed', type=int, default=42, help='seed')
     parser.add_argument('--model', type=str, default='resnet50', help='model')
+    parser.add_argument('--batch_size', type=str, default=256, help='model')
 
     args = parser.parse_args()
     return args
@@ -19,7 +20,7 @@ if __name__ == '__main__':
     args = main()
     set_seed(args.seed)
     model = build_classifier(args.model)
-    trainloader, testloader = build_dataloader()
+    trainloader, testloader = build_dataloader(batch_size=args.batch_size)
     train_feature, train_logits, train_labels = model.run_and_cache_outputs(trainloader)
     test_feature, test_logits, test_labels = model.run_and_cache_outputs(testloader, train=False)
     # estimator = KNNDistance()
