890630ca3ee701314bcce51c3e61a27db7b75903
Xi HuaJun <12112806@main.sustech.edu.cn>
2023-12-20 12:34:59 +0800

    main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from commons.utils import set_seed, evaluate
 from datasets.utils import build_dataloader
 from cache.utils import build_classifier
 from algorithms.estimator import KNNDistance, GMMAtypicalityEstimator
+from algorithms.calibrator import TemperatureScaling
 
 
 def main():
@@ -20,9 +21,17 @@ if __name__ == '__main__':
     args = main()
     set_seed(args.seed)
     model = build_classifier(args.model)
-    trainloader, testloader = build_dataloader(batch_size=args.batch_size)
+    trainloader, calibloder, testloader = build_dataloader(batch_size=args.batch_size)
     train_feature, train_logits, train_labels = model.run_and_cache_outputs(trainloader)
+    _, calib_logits, calib_labels = model.run_and_cache_outputs(calibloder)
     test_feature, test_logits, test_labels = model.run_and_cache_outputs(testloader, train=False)
+
+    # calibration
+    calibrator = TemperatureScaling()
+    calibrator.fit(calib_logits, calib_labels)
+    test_logits = calibrator.calibrate(test_logits, softmax=False)
+
+    # calculate atypicality
     estimator = KNNDistance()
     # estimator = GMMAtypicalityEstimator()
     estimator.fit(train_feature, train_labels)
