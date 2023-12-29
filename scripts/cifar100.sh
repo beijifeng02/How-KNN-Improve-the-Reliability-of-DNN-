21b93ee0e90145f9e0a3379254a453f3dacd70eb
Xi HuaJun <12112806@main.sustech.edu.cn>
2023-12-29 17:01:31 +0800

    message

--- scripts/cifar100.sh
+++ scripts/cifar100.sh
@@ -3,5 +3,5 @@ export CUDA_VISIBLE_DEVICES=1
 MODEL=("resnet18" "resnet50")
 
 for model in "${MODEL[@]}"; do
-  python main.py --model "${model}" --dataset "cifar10"
+  python main.py --model "${model}" --dataset "cifar100"
 done
--- scripts/svhn.sh
+++ scripts/svhn.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=1
 
-MODEL=("resnet18" "resnet50" "resnet101")
+MODEL=("resnet18" "resnet50")
 
 for model in "${MODEL[@]}"; do
   python main.py --model "${model}" --dataset "svhn"
